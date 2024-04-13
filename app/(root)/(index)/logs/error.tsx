93ec89bb40c73e8a160bf3772dc23801dfc59f16
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-04-13 16:42:45 -0300

    31: add new page route: /logs

---
+++ app/(root)/(index)/logs/error.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import AnimationError from "@/public/animation-error.json"
+import { AnimationLottie } from "@/components/animation-lottie"
+
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+
+    <section className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center px-8 py-24 mx-auto max-w-7xl">
+          <div className="text-center">
+            <div className="flex flex-col items-center">
+              <AnimationLottie animationPath={AnimationError}  />
+              <p className="mt-4 text-sm font-medium text-gray-500 text-center">
+                {error.message}
+              </p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-2 mt-8 md:flex-row">
+              <Button onClick={() => reset()} className="inline-flex items-center justify-center w-full h-12 gap-3 px-5 py-3 font-medium duration-200 bg-gray-100 md:w-auto rounded-xl hover:bg-gray-200 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" aria-label="Secondary action">
+                Reset
+              </Button>
+            </div>
+          </div>
+      </div>
+    </section>
+
+
+  )
+}
---
+++ app/(root)/(index)/logs/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import { useConvexAuth } from "convex/react";
+
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card";
+import { Loader } from "@/components/loader";
+import { Header } from "@/components/header";
+import { Sidebar } from "@/components/sidebar";
+import { CardHeaders } from "@/components/card-headers";
+import { InventoryTable } from "./_components/table/inventory-table";
+
+const InventoryPage = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (!isAuthenticated) {
+    redirect("/");
+  }
+
+  if (isLoading) <div className="flex h-screen items-center justify-center"><Loader text="Loading..." /></div>;
+
+  
+  return (
+    <div className="flex h-full w-full flex-col bg-muted/40">
+      <Sidebar />
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <Header />
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+          <Card>
+              <CardHeaders title="Inventory" description="Manage and keep track of your inventory."/>
+              <CardContent>
+                <InventoryTable />
+              </CardContent>
+          </Card>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default InventoryPage;
